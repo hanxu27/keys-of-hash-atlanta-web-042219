e8cca2a51faacf0a2577b57cadeffe0efca876e2
han xu <hanxu27@gmail.com>
2019-04-15 21:26:47 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,9 @@
 require "pry"
 
 class Hash
+  
+  hash = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"} 
+  
   def keys_of(*arguments)
     # code goes here
     binding.pry
